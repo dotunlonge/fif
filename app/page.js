c3d573ed817fe5997bf1d0b142c8b230884843f2
Dotun Longe <oludotunlonge@gmail.com>
2023-10-25 17:33:12 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
+"use client";
 import Center from './components/columns/center';
 import Left from './components/columns/left';
 import Right from './components/columns/right';
 import Navbar from './components/navbar';
 import Selector from './components/selector';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 export default function Home() {
+  
   return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
     <main className={"main"}>
       <Navbar/>
       <Selector/>
@@ -15,5 +20,6 @@ export default function Home() {
         <Right/>
       </div>
     </main>
+    </LocalizationProvider>
   )
 }
